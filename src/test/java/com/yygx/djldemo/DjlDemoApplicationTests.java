7a856323a24026d60d57e9326049a30c687420b0
吟风抱月 <3472151649.@qq.com>
2024-12-21 20:41:35 +0800

    初始化

---
+++ src/test/java/com/yygx/djldemo/DjlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yygx.djldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjlDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
